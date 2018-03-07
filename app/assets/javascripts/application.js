e941425b1d809a78bbc92d3c74adc60e7211ecd3
a3y3 <sohamssd@gmail.com>
2018-03-07 13:31:58 +0530

    Reorganize search bar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,24 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require js/bootstrap.min 
+//= require js/jquery.easing
+//= require js/owl.carousel.js
+//= require js/jquery-waypoints.js
+//= require js/imagesloaded.min.js
+//= require js/jquery.isotope.min.js
+//= require js/jquery-countTo.js
+//= require js/jquery.fancybox.js
+//= require js/jquery.flexslider-min.js
+//= require js/jquery.cookie.js
+//= require js/gmap3.min.js
+//= require js/jquery-validate.js
+//= require js/parallax.js
+//= require js/main.js
+//= require js/jquery.themepunch.tools.min.js
+//= require js/jquery.themepunch.revolution.min.js
+//= require js/slider.js
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
