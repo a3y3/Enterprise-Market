8b5317953071680ffd8e41e81e87ab358178c0c1
a3y3 <sohamssd@gmail.com>
2018-03-22 20:40:14 +0530

    Add collection_select to form

--- app/controllers/rfq_controller.rb
+++ app/controllers/rfq_controller.rb
@@ -1,4 +1,27 @@
 class RfqController < ApplicationController
 	def new
+		@commodities = CommodityType.all
+	end
+	def create
+		rfq = Rfq.new(rfq_params)
+		if rfq.save
+			flash[:notice] = "Your request has been created."
+			redirect_to root_path
+		else
+			flash[:alert] = "There was an error. Please contact support."
+			redirect_to request.referrer
+		end
+	end
+	private
+	def rfq_params
+		if(params[:rfq][:commodity] == "")
+		end
+		params.require(:rfq).permit(:commodity_type_id,
+																:description, 
+																:quantity,
+																:uom,
+																:pincode,
+																:mobile_number).merge(:pending => true)
+
 	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+CommodityType.create(name: "Steel", available_quantity: 23)
+CommodityType.create(name: "Polymer", available_quantity: 23)
+CommodityType.create(name: "Avalon", available_quantity: 23)
+CommodityType.create(name: "Iron", available_quantity: 23)
+CommodityType.create(name: "Zinc", available_quantity: 23)
+CommodityType.create(name: "Gold", available_quantity: 23)
+CommodityType.create(name: "Tires", available_quantity: 23)
+CommodityType.create(name: "Aluminium", available_quantity: 23)
+CommodityType.create(name: "Copper", available_quantity: 23)
