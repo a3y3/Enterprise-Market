258b56690a9cf073a7e849716e65dd2409114f96
a3y3 <sohamssd@gmail.com>
2018-03-26 13:33:31 +0530

    Change success paths

--- app/controllers/rfq_controller.rb
+++ app/controllers/rfq_controller.rb
@@ -4,9 +4,8 @@ class RfqController < ApplicationController
 	end
 	def create
 		rfq = Rfq.new(rfq_params)
-		if rfq.save
-			flash[:notice] = "Your request has been created."
-			redirect_to root_path
+		if rfq.save!
+			redirect_to rfq_success_path
 		else
 			flash[:alert] = "There was an error. Please contact support."
 			redirect_to request.referrer
@@ -14,8 +13,6 @@ class RfqController < ApplicationController
 	end
 	private
 	def rfq_params
-		if(params[:rfq][:commodity] == "")
-		end
 		params.require(:rfq).permit(:commodity_type_id,
 																:description, 
 																:quantity,
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
   get 'welcome/index'
+
   resources :rfq, :except => [:show]
+  get 'rfq/success' => 'rfq#success', as: 'rfq_success'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
