7ab4937b2f1b89ad09445000d670cb81d2b4f245
a3y3 <sohamssd@gmail.com>
2018-03-28 17:01:03 +0530

    Add Petergate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180328110111) do
+ActiveRecord::Schema.define(version: 20180328113031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20180328110111) do
     t.text "last_name"
     t.text "company_name"
     t.integer "mobile_number"
+    t.string "roles"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
